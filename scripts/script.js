396cbce28055fbd152e5cb359b7d484ca586ca4c
Lloyd456 <darten456@gmail.com>
2022-07-02 17:07:25 +0300

    Пофикшены ошибки после тестов яндекса

--- scripts/script.js
+++ scripts/script.js
@@ -20,29 +20,20 @@ popupKrest.addEventListener('click', popupClose);
 //Реализация изменений в профиле
 
 // Находим форму в DOM
-let formElement = popup.querySelector('.popup__form'); // Воспользуйтесь методом querySelector()
+let formElement = popup.querySelector('.popup__form');
 // Находим поля формы в DOM
-let nameInput = formElement.querySelector('#nameInput');// Воспользуйтесь инструментом .querySelector()
-let jobInput = formElement.querySelector('#jobInput'); // Воспользуйтесь инструментом .querySelector()
+let nameInput = formElement.querySelector('#nameInput');
+let jobInput = formElement.querySelector('#jobInput');
 
-// Обработчик «отправки» формы, хотя пока
-// она никуда отправляться не будет
 function formSubmitHandler (evt) {
-    evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
-                                                // Так мы можем определить свою логику отправки.
-                                                // О том, как это делать, расскажем позже.
-
-    // Получите значение полей jobInput и nameInput из свойства value
+    evt.preventDefault();
     let name = nameInput.value
     let profession = jobInput.value
-    // Выберите элементы, куда должны быть вставлены значения полей
     let newName = profile.querySelector('.profile__name');
     let newJob = profile.querySelector('.profile__profession');
-    // Вставьте новые значения с помощью textContent
     newName.textContent = name;
     newJob.textContent = profession;
 }
 
 // Прикрепляем обработчик к форме:
-// он будет следить за событием “submit” - «отправка»
 formElement.addEventListener('submit', formSubmitHandler);
